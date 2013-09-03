717825cb256ad8d3e3bac89d8df0b35058426641
Masse, John <john.masse@priceline.com>
2013-09-03 17:27:39 -0400

    notes from a demo

--- client/views/left_nav/left_nav.js
+++ client/views/left_nav/left_nav.js
@@ -1,18 +1,17 @@
-$(document).ready(function(){
-	var leftNav = {}; // left nav namespace
+var leftNav = {}; // left nav namespace
 
+leftNav.control = { // methods for ui elements
+	addNewRow: function(event, t) {
+		event.preventDefault();
+		RowData.insert(globalValues.baseRowObject); // add a new row to the row data store
+	},
+	addNewSection: function(event, t) {
+		event.preventDefault();
+		console.log('Build a new section');
+	}
+};
 
-	leftNav.control = { // methods for ui elements
-		addNewRow: function(event, t) {
-			event.preventDefault();
-			RowData.insert(globalValues.baseRowObject); // add a new row to the row data store
-		},
-		addNewSection: function(event, t) {
-			event.preventDefault();
-			console.log('Build a new section');
-		}
-	};
-
+$(document).ready(function() {
 	Template.left_nav.events({ // assigning events to the template
 		'click #btn_add_item': leftNav.control.addNewRow,
 		'click #btn_create_section': leftNav.control.addNewSection
