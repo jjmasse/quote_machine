d30c0bff52bf4530c0d4da7d30c0a8543c8dd788
Masse, John <john.masse@priceline.com>
2013-09-03 15:01:11 -0400

    poking around with looping on the amount result

--- client/views/total_info/total_info.js
+++ client/views/total_info/total_info.js
@@ -3,9 +3,15 @@ $(document).ready(function() {
 
 	// build out the sum of all amounts in stored rows and add them here
 
-	var query = ApplicationData.find({}).total_amount;
+	var query = ApplicationData.find({});
+
+	query.forEach(function(row) {
+		console.log(row);
+	});
 
 	Template.total_info.total = function() {
-		return ApplicationData.find({}).fetch().total_amount;
+		var test = '100.00';
+		return test;
+		//return ApplicationData.find({}).fetch().total_amount;
 	}
 });
