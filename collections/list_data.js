35a24dafa521fe3f434eff7159a86018b4b22ed0
John Masse <john@jjmasse.com>
2013-09-04 21:38:03 -0400

    Removed un-needed Meteor Collection

--- collections/list_data.js
+++ collections/list_data.js
@@ -4,15 +4,5 @@ RowData = new Meteor.Collection(); // creates an expiring mini-mongo db
 // insert some dummy data
 RowData.insert(globalValues.baseRowObject);
 
-// application wide data
-ApplicationData = new Meteor.Collection();
-
-// default application data obj
-DefaultApplicationData = {
-	total_amount: '100.00'
-};
-
-ApplicationData.insert(DefaultApplicationData); // add the default application data to the collection
-
 
 
