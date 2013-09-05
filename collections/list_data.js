6bb68cbdc89335cf45b604794c5a72ed8ef1b04e
John Masse <john@jjmasse.com>
2013-09-05 18:15:17 -0400

    1) Messing around with route configurations 2) Working on stubbing out some additional route logic 3) Starting to work on persistant data collection

--- collections/list_data.js
+++ collections/list_data.js
@@ -1,8 +1,12 @@
 // create row data
-RowData = new Meteor.Collection(); // creates an expiring mini-mongo db
+// RowData = new Meteor.Collection(); // creates an expiring mini-mongo db
+RowData = new Meteor.Collection('quotes'); // get the quotes collection
+
+console.log(RowData.find().count());
+
 
 // insert some dummy data
-RowData.insert(globalValues.baseRowObject);
+// RowData.insert(globalValues.baseRowObject);
 
 
 
