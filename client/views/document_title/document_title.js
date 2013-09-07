c4c665554d8a77cccb6c360f8b86bea1e028f7bc
John Masse <john@jjmasse.com>
2013-09-07 18:51:14 -0400

    1) Removing all text from the title field will yield an untitled title 2) Create quotes as you see fit

--- client/views/document_title/document_title.js
+++ client/views/document_title/document_title.js
@@ -5,7 +5,9 @@ documentTitle.controls = {
 	updateDocumentTitle: function(event, template) {
 		// takes the entered text and saves it as the documents title
 		var newText = event.currentTarget.value; // get the value the peeps entered
-
+		if(newText === '') { // if the thing comes out blank then make it untitled
+			newText = 'untitled';
+		}
 		DocumentData.update({_id: Session.get('document')}, {$set: {title: newText}});
 	}
 };
--- client/views/nav/nav.js
+++ client/views/nav/nav.js
@@ -6,11 +6,17 @@ nav.controls = {
 		event.preventDefault();
 		// fire off the warning modal
 		$('#warn_window').modal(); // big trick l0l...
+	},
+	createBlankQuote: function(event, template) {
+		event.preventDefault();
+		// create a new blank doc and move the user over to it
+		GlobalHelpers.generateNewBlankQuote();
 	}
 };
 
 $(document).ready(function() { // we need a bit of jQuery for this to call upon the warning modal
 	Template.nav.events({
-		'click #btn_warn_user_of_delete': nav.controls.warnUser
+		'click #btn_warn_user_of_delete': nav.controls.warnUser,
+		'click #btn_create_new_blank_quote': nav.controls.createBlankQuote
 	});
 });
