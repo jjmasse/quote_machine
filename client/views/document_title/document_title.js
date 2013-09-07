f280e7dfe2aa78c2acfc856cad3486cba923c648
John Masse <john@jjmasse.com>
2013-09-07 18:02:39 -0400

    App is getting crazy
    1) Titles can now be updated...

---
+++ client/views/document_title/document_title.js
@@ -0,0 +1,21 @@
+// namespace for this piece
+var documentTitle = {};
+documentTitle.data = {};
+documentTitle.controls = {
+	updateDocumentTitle: function(event, template) {
+		// takes the entered text and saves it as the documents title
+		var newText = event.currentTarget.value; // get the value the peeps entered
+
+		DocumentData.update({_id: Session.get('document')}, {$set: {title: newText}});
+	}
+};
+
+// get the documents title into the text field
+Template.document_title.documentData = function() {
+	return DocumentData.findOne({_id: Session.get('document')});
+};
+
+// do the event thing here
+Template.document_title.events({
+	'keyup #document_title_input': documentTitle.controls.updateDocumentTitle
+});
