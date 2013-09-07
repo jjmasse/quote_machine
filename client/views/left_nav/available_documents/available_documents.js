2ac3501bcae242984b9638cde3d02bdb02a43569
John Masse <john@jjmasse.com>
2013-09-07 17:29:05 -0400

    1) New view - focused on listing out all of the documents available 2) New view has its events stubbed out - need further refinement and testing 3) Selected content is being passed along to the document

---
+++ client/views/left_nav/available_documents/available_documents.js
@@ -0,0 +1,21 @@
+// setup the templates namespace
+var availableDocuments = {};
+availableDocuments.data = {};
+availableDocuments.controls = {
+	switchDocument: function(event, template) { // resets the session to load up the selected documents content
+		event.preventDefault();
+		console.log('updating the document session id to', Session.get('document'));
+		Session.set({'document': 'this._id'});
+	}
+};
+
+
+// get the list of documents out to the template
+Template.available_documents.documentList = function() {
+	return DocumentData.find({}); // return all of the documents
+};
+
+// time for some events
+Template.available_documents.events({
+	'click .btn_set_document': availableDocuments.controls.switchDocument
+});
