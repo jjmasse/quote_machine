d54824cb31c665c2f2c1f04028470a5ddba8b3f6
Masse, John <john.masse@priceline.com>
2013-09-09 10:40:58 -0400

    fixed the math issue in the total row

--- client/views/total_info/total_info.js
+++ client/views/total_info/total_info.js
@@ -1,37 +1,35 @@
 // js for total info row
-$(document).ready(function() {
-	var totalInfo = {};
+var totalInfo = {};
 
-	totalInfo.data = { // stores any data we need for total info
-		totalAmount: 0
-	};
+totalInfo.data = { // stores any data we need for total info
+	totalAmount: 0
+};
 
-	totalInfo.control = { // total info controller methods
-		updateTotalAmount: function() { // whenever an amount changes - we re-run the generation of the amount
-			// loop through all entries associated with this document
-			var rows = RowData.find({associatedDocument: Session.get('document')}),
-				newTotal = 0; // prepare the var that will become the new value
-			rows.forEach(function(row) {
-				var rowAmt = row.dollaAmount, // store the amount
-					rowOpperationSymbol = row.opperation; // sums up the new total
-				if(rowAmt !== '') {
-					if(rowOpperationSymbol === '+') {
-						newTotal = parseFloat(newTotal) + parseFloat(rowAmt);
-					} else if(rowOpperationSymbol === '-') {
-						newTotal = parseFloat(newTotal) - parseFloat(rowAmt);
-					}
+totalInfo.control = { // total info controller methods
+	updateTotalAmount: function() { // whenever an amount changes - we re-run the generation of the amount
+		// loop through all entries associated with this document
+		var rows = RowData.find({associatedDocument: Session.get('document')}),
+			newTotal = 0; // prepare the var that will become the new value
+			
+		// loops through all of the rows and creates a new sum
+		rows.forEach(function(row) {
+			var rowAmt = row.dollaAmount, // store the amount
+				rowOpperationSymbol = row.opperation; // sums up the new total
+			if(rowAmt !== '') {
+				if(rowOpperationSymbol === '+') {
+					newTotal = parseFloat(newTotal) + parseFloat(rowAmt);
+				} else if(rowOpperationSymbol === '-') {
+					newTotal = parseFloat(newTotal) - parseFloat(rowAmt);
 				}
-			});
-			// pass off the new total
-			return newTotal.toFixed(2);
-		}
-	};
-
-
-	// meteor templates are re-evaluated whenever something changes
-	Template.total_info.totalAmount = function() {
-		totalInfo.data.totalAmount = totalInfo.control.updateTotalAmount();
-		return totalInfo.data.totalAmount;
+			}
+		});
+		// pass off the new total
+		return newTotal.toFixed(2);
 	}
-	
-});
+};
+
+// meteor templates are re-evaluated whenever something changes
+Template.total_info.totalAmount = function() {
+	totalInfo.data.totalAmount = totalInfo.control.updateTotalAmount();
+	return totalInfo.data.totalAmount;
+}
